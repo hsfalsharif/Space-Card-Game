f21cd42731dedbd8da93ee34bf41ba68c0547c8e
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-10-01 19:55:38 +0300

    update fuel and mass from class effect funcs

--- effect_funcs.py
+++ effect_funcs.py
@@ -1,54 +1,63 @@
+from _typeshed import Self
+import player
+
+player = player.Player()
+
+
 class Effect_Function:
 
-    def __init__(self, current_fuel, current_mass):
-        self.current_fuel = current_fuel
-        self.current_mass = current_mass
 
+    def __init__(self, fuel, mass):
+        self.fuel = player.get_fuel()
+        self.mass = player.get_mass()
+        
+
+    @staticmethod
     def igniter(self):
-        self.current_fuel -= 4
-        self.current_mass -= 3
+        player.set_fuel(self.fuel - 4)
+        player.set_mass(self.mass + 3)
 
     def thruster(self):
-        self.current_fuel -= 3
-        self.current_mass -= 3
+        player.set_fuel(self.fuel - 3)
+        player.set_mass(self.mass + 3)
 
     def solid_rocket_booster(self):
-        self.current_fuel -= 1
-        self.current_mass -= 3
+        player.set_fuel(self.fuel - 1)
+        player.set_mass(self.mass + 3)
 
     def landing_gear(self):
-        self.current_fuel -= 0
-        self.current_mass -= 2
+        player.set_fuel(self.fuel - 0)
+        player.set_mass(self.mass + 2)
 
     def rcs_thrusters(self):
-        self.current_fuel -= 2
-        self.current_mass -= 1
+        player.set_fuel(self.fuel - 2)
+        player.set_mass(self.mass + 1)
 
     def thruster_gimbel(self):
-        self.current_fuel -= 4
-        self.current_mass -= 3
+        player.set_fuel(self.fuel - 4)
+        player.set_mass(self.mass + 3)
 
     def electric_propulsion(self):
-        self.current_fuel -= 0
-        self.current_mass -= 6
+        player.set_fuel(self.fuel - 0)
+        player.set_mass(self.mass + 6)
 
     def decoupler(self):
-        self.current_fuel -= 2
-        self.current_mass -= 2
+        player.set_fuel(self.fuel - 2)
+        player.set_mass(self.mass + 2)
 
     def accelerometer(self):
-        self.current_fuel -= 0
-        self.current_mass -= 2
+        player.set_fuel(self.fuel - 0)
+        player.set_mass(self.mass + 2)
 
 
     def ring_laser_gyroscope(self):
-        self.current_fuel -= 1
-        self.current_mass -= 2
+        player.set_fuel(self.fuel - 1)
+        player.set_mass(self.mass + 2)
 
     def star_tracker(self):
-        self.current_fuel -= 1
-        self.current_mass -= 4
+        player.set_fuel(self.fuel - 1)
+        player.set_mass(self.mass + 4)
     
     def fuel_tank(self):
-        self.current_fuel -= 0
-        self.current_mass -= 3
+        player.set_fuel(self.fuel - 0)
+        player.set_mass(self.mass + 3)
