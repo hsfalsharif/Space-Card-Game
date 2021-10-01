4878ab0479cda4a2ca870c92071264136ec9dce3
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-10-01 19:39:54 +0300

    create player class with initial params

---
+++ player.py
@@ -0,0 +1,115 @@
+class Player:
+
+    def __init__(self):
+        self.fuel = 20
+        self.mass = 10
+        self.number_of_igniter_card = 1
+        self.number_of_fire_engine_card = 3
+        self.number_of_rocket_booster_card = 0
+        self.number_of_landing_gear_card = 1
+        self.number_of_RCS_thrusters_card = 0
+        self.number_of_electric_propulsion_card = 0
+        self.number_of_decoupler_card = 0
+        self.number_of_accelerometer_card = 0
+        self.number_of_ring_laser_gyroscope_card = 0
+        self.number_of_star_tracker_card = 0
+        self.number_of_fuel_tank_card = 0
+        self.number_of_airbags_card = 0
+
+    # mass
+    def set_mass(self, mass):
+        self.mass = mass
+    
+    def get_mass(self):
+        return self.mass
+    
+    # fuel
+    def set_fuel(self, fuel):
+        self.fuel = fuel
+    
+    def get_fuel(self):
+        return self.fuel
+    
+    # number_of_igniter_card
+    def set_number_of_igniter_card(self, number_of_igniter_card):
+        self.number_of_igniter_card = number_of_igniter_card
+    
+    def get_number_of_igniter_card(self):
+        return self.number_of_igniter_card
+    
+    # number_of_fire_engine
+    def set_number_of_fire_engine_card(self, number_of_fire_engine_card):
+        self.number_of_fire_engine_card = number_of_fire_engine_card
+    
+    def get_number_of_fire_engine_card(self):
+        return self.number_of_fire_engine_card
+    
+    # number_of_rocket_booster_card
+    def set_number_of_rocket_booster_card(self, number_of_rocket_booster_card):
+        self.number_of_rocket_booster_card = number_of_rocket_booster_card
+    
+    def get_number_of_rocket_booster_card(self):
+        return self.number_of_rocket_booster_card
+
+    # number_of_landing_gear_card
+    def set_number_of_landing_gear_card(self, number_of_landing_gear_card):
+        self.number_of_landing_gear_card = number_of_landing_gear_card
+    
+    def get_number_of_landing_gear_card(self):
+        return self.number_of_landing_gear_card
+
+    # number_of_RCS_thrusters_card
+    def set_number_of_RCS_thrusters_card(self, number_of_RCS_thrusters_card):
+        self.number_of_RCS_thrusters_card = number_of_RCS_thrusters_card
+    
+    def get_number_of_RCS_thrusters_card(self):
+        return self.number_of_RCS_thrusters_card
+
+    # number_of_electric_propulsion_card
+    def set_number_of_electric_propulsion_card(self, number_of_electric_propulsion_card):
+        self.number_of_electric_propulsion_card = number_of_electric_propulsion_card
+    
+    def get_number_of_electric_propulsion_card(self):
+        return self.number_of_electric_propulsion_card
+
+    # number_of_decoupler_card
+    def set_number_of_decoupler_card(self, number_of_decoupler_card):
+        self.number_of_decoupler_card = number_of_decoupler_card
+    
+    def get_number_of_decoupler_card(self):
+        return self.number_of_decoupler_card
+
+    # number_of_accelerometer_card
+    def set_number_of_accelerometer_card(self, number_of_accelerometer_card):
+        self.number_of_accelerometer_card = number_of_accelerometer_card
+    
+    def get_number_of_accelerometer_card(self):
+        return self.number_of_accelerometer_card
+
+    # number_of_ring_laser_gyroscope_card
+    def set_number_of_ring_laser_gyroscope_card(self, number_of_ring_laser_gyroscope_card):
+        self.number_of_ring_laser_gyroscope_card = number_of_ring_laser_gyroscope_card
+    
+    def get_number_of_ring_laser_gyroscope_card(self):
+        return self.number_of_ring_laser_gyroscope_card
+
+    # number_of_star_tracker_card
+    def set_number_of_star_tracker_card(self, number_of_star_tracker_card):
+        self.number_of_star_tracker_card = number_of_star_tracker_card
+    
+    def get_number_of_star_tracker_card(self):
+        return self.number_of_star_tracker_card
+
+    # number_of_fuel_tank_card
+    def set_number_of_fuel_tank_card(self, number_of_fuel_tank_card):
+        self.number_of_fuel_tank_card = number_of_fuel_tank_card
+    
+    def get_number_of_fuel_tank_card(self):
+        return self.number_of_fuel_tank_card
+
+    # number_of_airbags_card
+    def set_number_of_airbags_card(self, number_of_airbags_card):
+        self.number_of_airbags_card = number_of_airbags_card
+    
+    def get_number_of_airbags_card(self):
+        return self.number_of_airbags_card
