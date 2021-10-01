b479db82ad3c1f7745cb3b216d162a22bcda6c6b
Sultanmahfouz <63063709+Sultanmahfouz@users.noreply.github.com>
2021-10-01 16:07:25 +0300

    define a list of all possible cards

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ current_fuel = 100
 current_mass = 10
 
 def empty_func():
-    print('Nothing')
+    pass
 
 igniter_card = card.Card('igniter', empty_func())
 fire_engine_card = card.Card('fire_engine', empty_func())
@@ -21,3 +21,14 @@ landing_gear_card = card.Card('landing_gear', empty_func())
 rcs_thrusters = card.Card('rcs_thrusters', empty_func())
 thruster_gimbel_card = card.Card('thruster_gimbel', empty_func())
 electric_propulsion_card = card.Card('electric_propulsion', empty_func())
+
+
+list_of_cards = [
+    igniter_card,
+    fire_engine_card,
+    solid_rocket_booster_card,
+    landing_gear_card,
+    rcs_thrusters,
+    thruster_gimbel_card,
+    electric_propulsion_card,
+]
